7e7b31858a2aa04c2888005fb998b5d7a0a6b0bd
zhiwen.li <674356245@qq.com>
2022-12-30 16:39:56 +0800

    add odin inspector

---
+++ Assets/Test/Test.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [LabelText("列表")]
+    public List<int> list;
+}
